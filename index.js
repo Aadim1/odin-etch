2593873ef56ee22b97fe3f7640bcbf8628b24639
Aadim Chaulagain <iamaadim5@gmail.com>
2021-11-19 20:45:01 -0800

    Finished project

--- index.js
+++ index.js
@@ -1,30 +1,92 @@
 let element = document.querySelector('#root');
-
-
+let radioCheck = document.getElementsByName('select');
+let color = 'black';
+let grid_ = 16;
+let rainb = false;
 
 // Creates a 16X16 grid. Will be dynamic later. 
-for (let i = 0; i < 16; i++) {
-    let grid = document.createElement('div');
-    element.appendChild(grid);
-    grid.classList.toggle('grid');
-    for (let i = 0; i < 16; i++) {
-        let child = document.createElement('div');
-        child.classList.toggle('child');
-        grid.appendChild(child);
+
+const createGrid = () => {
+    for (let i = 0; i < grid_; i++) {
+        let grid = document.createElement('div');
+        element.appendChild(grid);
+        grid.classList.toggle('grid');
+        for (let i = 0; i < grid_; i++) {
+            let child = document.createElement('div');
+            child.classList.toggle('child');
+            grid.appendChild(child);
+        };
     };
 };
 
 // Adds a event listener to see if the user hovers over one of the element
-let child = document.querySelectorAll('.child');
-console.log(child);
-child.forEach((sketch) => {
-    sketch.addEventListener("mouseover", function(event) {
-        event.target.style['background-color'] = 'black';
+
+const bgChange = () => {
+    let child = document.querySelectorAll('.child');
+    child.forEach((sketch) => {
+        sketch.addEventListener("mouseover", function(event) {
+            event.target.style['background-color'] = color;
+        });
     });
-});
+};
+
+createGrid();
+bgChange();
+
+const removeGrid = () => {
+    let grid = document.querySelectorAll('.grid');
+    let child = document.querySelectorAll('.child');
+
+    grid.forEach((element) => element.remove());
+    child.forEach((element) => element.remove())
+}
+
+const changeGrid = (p) => {
+    removeGrid();
+    grid_ = p;
+    createGrid();
+    bgChange();
+    console.log()
+}
+
+const removeInterval = () => {
+    if (window.myInterval != undefined && window.myInterval != 'undefined') {
+        window.clearInterval(window.myInterval);
+    }
+}
+
+
+function random_rgba() {
+    removeInterval()
+    var o = Math.round,
+        r = Math.random,
+        s = 255;
+    color = 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
+}
+
+const rainbow = () => {
+    window.myInterval = setInterval(
+        function() {
+            var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            color = "#" + randomColor;
+        }, 1000);
+}
+
+
+
+
 
 const clearAll = () => {
+    let child = document.querySelectorAll('.child');
     child.forEach((sketch) => {
-        sketch.style['background-color'] = 'white';
+        sketch.style['background-color'] = '#f8eedb'
     })
-}
+};
+
+let color_picker = document.getElementById("colorssss");
+color_picker.addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+    console.log(event.target.value)
+    color = event.target.value;
+};
